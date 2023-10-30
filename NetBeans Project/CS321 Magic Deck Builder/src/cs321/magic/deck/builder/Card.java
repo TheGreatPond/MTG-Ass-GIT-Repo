e9a827cd12e2140a403edfc563917fd23df40bd9
Adam <Adam@DESKTOP-L0G97EJ.attlocal.net>
2023-10-30 17:06:22 -0500

    Created comments for JAVA doc in all but the App.class fixed some saving errors in the deck json files

--- NetBeans Project/CS321 Magic Deck Builder/src/cs321/magic/deck/builder/Card.java
+++ NetBeans Project/CS321 Magic Deck Builder/src/cs321/magic/deck/builder/Card.java
@@ -1,20 +1,63 @@
+/**
+ * File originally created and commented by Adam Pierce
+ * 
+ */
+
 package cs321.magic.deck.builder;
 
 import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Card Class stores cardID, name, card type, mana cost, total Mana cost, description, and file path to the image file
+ *
+ */
 public class Card {
+    /**
+     * Creates a hash map to store the card objects
+     */
     private static Map<Integer, Card> allCards = new HashMap<>(); // Map to store all card objects
-
-    private final int cardID; // Represents the multiverseid
-    private final String name;
-    private final String cardType;  // e.g., creature, spell, land
-    private final String manaCost;  // e.g., 1UU, 2R
-    private final int totalMana;    // Previously named 'cmc'
-    private final String description;  // Represents the text
-    private final String imageFile;    // Previously named 'imageUrl'
     
+    /**
+     * uses the multiverse ID of each card as an unique identifier
+     */
+    private final int cardID;
+    
+    /**
+     * name of card
+     */
+    private final String name;
+    
+    /**
+     * type of the card, e.g. creature, spell, land 
+     */
+    private final String cardType;
+    
+    /**
+     * mana cost of the card, e.g. 1UU, 2R
+     */
+    private final String manaCost;
+    
+    /**
+     * total mana of the card or 'cmc' for converted mana cost
+     */
+    private final int totalMana;
+    
+    /**
+     * the card description
+     */
+    private final String description;
+    
+    /**
+     * stores the location of the image file for the card
+     */
+    private final String imageFile;
+    
+    /**
+     * This function builds the card parameters using the object imported by the jsonjava library
+     * @param jsonObject 
+     */
     public Card(JSONObject jsonObject) {
         this.cardID = jsonObject.optInt("multiverseid", 0);
         this.name = jsonObject.getString("name");
@@ -27,35 +70,67 @@ public class Card {
         allCards.put(cardID, this); // Add card to the map
     }
 
-    // Static method to get a Card by its ID
+    /**
+     * Static method to return a card by it's ID
+     * @param cardID
+     * @return allCards.get(cardID)
+     */
     public static Card getCardFromID(int cardID) {
         return allCards.get(cardID);
         
-    }    // Getter methods
+    }
+    /**
+     * returns the card ID
+     * @return cardID
+     */
     public int getCardID() {
         return cardID;
     }
-
+    
+    /**
+     * Returns the name of the card
+     * @return name
+     */
     public String getName() {
         return name;
     }
-
+    
+    /**
+     * Returns the card type
+     * @return cardType
+     */
     public String getCardType() {
         return cardType;
     }
-
+    
+    /**
+     * Returns the Mana cost of the card
+     * @return manaCost
+     */
     public String getManaCost() {
         return manaCost;
     }
-
+    
+    /**
+     * returns the total mana of the card
+     * @return totalMana
+     */
     public int getTotalMana() {
         return totalMana;
     }
-
+    
+    /**
+     * returns the description of the card
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
-
+    
+    /**
+     * returns the location of the image file of the card
+     * @return imageFile
+     */
     public String getImageFile() {
         return imageFile;
     }
--- NetBeans Project/CS321 Magic Deck Builder/src/cs321/magic/deck/builder/DeckLoader.java
+++ NetBeans Project/CS321 Magic Deck Builder/src/cs321/magic/deck/builder/DeckLoader.java
@@ -1,3 +1,7 @@
+/**
+ * File originally created and commented by Adam Pierce
+ * 
+ */
 package cs321.magic.deck.builder;
 
 import java.io.File;
@@ -7,43 +11,69 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * Class contains utility functions needed for loading and saving data to the JSON file
+ * 
+ */
 public class DeckLoader {
     private static final String DECKS_DIRECTORY = "src/mtg_data/";
     private final List<Deck> loadedDecks;
 
+    /**
+     * Constructor for the DeckLoader class.
+     * <p>
+     * Creates a new array to store which decks have been loaded
+     * Calls a function to load the decks saved in the provided directory
+     * </p>
+     */
     public DeckLoader() {
         loadedDecks = new ArrayList<>();
         loadDecksFromDirectory();
     }
 
+    /**
+     * Uses file stream to load all off the JSON files in the mtg_data folder that store deck data.
+     * <p>
+     * Calls the loadDeck(File file) function to load each deck found in directory
+     * </p>
+     */
     private void loadDecksFromDirectory() {
     try {
         Files.newDirectoryStream(Paths.get(DECKS_DIRECTORY),
             path -> path.toString().endsWith(".json") && !path.getFileName().toString().equals("WAR_cards.json"))
             .forEach(filePath -> loadDeck(filePath.toFile()));
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-
-    private void loadDeck(File file) {
-    try {
-        String jsonContent = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
-        JSONObject jsonObject = new JSONObject(jsonContent);
-        Deck deck = parseDeck(jsonObject);
-        loadedDecks.add(deck);
-    } catch (JSONException je) {
-        System.err.println("Error parsing JSON from file: " + file.getName());
-        je.printStackTrace();
     } catch (IOException e) {
         e.printStackTrace();
     }
 }
+
+    /**
+     * Uses the file parameter to create a JSON object 
+     * @param file 
+     */
+    private void loadDeck(File file) {
+        try {
+            String jsonContent = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+            JSONObject jsonObject = new JSONObject(jsonContent);
+            Deck deck = parseDeck(jsonObject);
+            loadedDecks.add(deck);
+        } catch (JSONException je) {
+            System.err.println("Error parsing JSON from file: " + file.getName());
+            je.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Analyzes the JSON object in the parameter to get the ID and quantity of the cards stored in the JSON file.
+     * @param jsonObject
+     * @return deck
+     */
     private Deck parseDeck(JSONObject jsonObject) {
         String name = jsonObject.getString("name");
         long hashID = jsonObject.getLong("hashID");
@@ -64,10 +94,19 @@ public class DeckLoader {
         return deck;
     }
 
+    /**
+     * Returns a copy of the current list of deck objects to prevent external modifications
+     * @return ArrayList(loadedDecks)
+     */
     public List<Deck> getDecks() {
-        return new ArrayList<>(loadedDecks); // Return a copy to prevent external modifications
+        return new ArrayList<>(loadedDecks);
     }
 
+    /**
+     * Filters and returns the deck that matches the name in the parameter
+     * @param name
+     * @return deck object
+     */
     public Deck getDeckByName(String name) {
         return loadedDecks.stream()
                           .filter(deck -> deck.getName().equals(name))
@@ -75,6 +114,11 @@ public class DeckLoader {
                           .orElse(null);
     }
     
+    /**
+     * Filters and returns the deck that matches the hash ID in the parameter
+     * @param hashID
+     * @return deck object
+     */
      public Deck getDeckByHashID(long hashID) {
         return loadedDecks.stream()
                           .filter(deck -> deck.getHashID() == hashID)
@@ -82,10 +126,18 @@ public class DeckLoader {
                           .orElse(null);
     }
     
+    /**
+     * Calls the saveToJson() function from the deck class to update the JSON files with the applied changes
+     * @param deck 
+     */
     public void saveDeck(Deck deck) {
         deck.saveToJson(); // Let the Deck instance handle its own JSON serialization
     }
 
+    /**
+     * Removes the deck object from the list and the directory
+     * @param deck 
+     */
     public void deleteDeck(Deck deck) {
     String filename = "deck_" + deck.getHashID() + ".json";
     File file = new File(DECKS_DIRECTORY + filename);
